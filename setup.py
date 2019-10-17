a8af10311124e2df51d0dc9a58f8f4415ccdbaae
Andrew O'Brien <aob@redballoonsecurity.com>
2019-10-17 14:02:09 -0400

    Fix requirement versions for Python2

--- setup.py
+++ setup.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python
 
 from setuptools import setup, find_packages
-from codecs     import open
+from codecs import open
+
+REQUIREMENTS = [
+    'networkx==2.2',
+    'elementals',
+    'sark',
+    'pydocstyle==3.0.0',
+    'flake8',
+    'click',
+    'numpy==1.16.5',
+    'scipy==1.2.2',
+    'scikit-learn==0.20.3',
+]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name='Karta',
-      version='1.2.0',
-      description='IDA plugin for identifying and matching open-source libraries in (huge) binaries',
-      author='Eyal Itkin',
-      author_email='eyalit@checkpoint.com',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url='https://github.com/CheckPointSW/Karta',
-      license='MIT',
-      packages=find_packages(),
-      install_requires=['elementals', 'sark', 'pydocstyle', 'flake8', 'click', 'scikit-learn==0.20.3'],
-      classifiers=[
-                    "Programming Language :: Python :: 2",
-                    "License :: OSI Approved :: MIT License (MIT License)",
-                    "Operating System :: OS Independent",
-                  ],
-      zip_safe=False)
+setup(
+    name='Karta',
+    version='1.2.0',
+    description='IDA plugin for identifying and matching open-source libraries in (huge) '
+                'binaries',
+    author='Eyal Itkin',
+    author_email='eyalit@checkpoint.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/CheckPointSW/Karta',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=REQUIREMENTS,
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License (MIT License)",
+        "Operating System :: OS Independent",
+    ],
+    zip_safe=False
+)
